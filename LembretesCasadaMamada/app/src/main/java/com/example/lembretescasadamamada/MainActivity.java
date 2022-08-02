054213bbd9b229a59c2e7ea23d9a50b09ac4c0f7
danielmodesto <daniel.modesto.dmds@gmail.com>
2022-08-02 16:07:13 -0400

    Lab 6 - Etapas obrigatorias

--- LembretesCasadaMamada/app/src/main/java/com/example/lembretescasadamamada/MainActivity.java
+++ LembretesCasadaMamada/app/src/main/java/com/example/lembretescasadamamada/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.lembretescasadamamada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoEntrar;
+    private EditText caixaNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setBotaoEntrar(findViewById(R.id.botaoEntrar));
+        this.setCaixaNome(findViewById(R.id.caixaNome));
+    }
+
+    public Button getBotaoEntrar() {
+        return botaoEntrar;
+    }
+
+    public void setBotaoEntrar(Button botaoEntrar) {
+        this.botaoEntrar = botaoEntrar;
+    }
+
+    public EditText getCaixaNome() {
+        return caixaNome;
+    }
+
+    public void setCaixaNome(EditText caixaNome) {
+        this.caixaNome = caixaNome;
+    }
+
+    public void entrarApp(View view){
+        Intent nextActivity = new Intent(MainActivity.this, TelaLembrete.class);
+        nextActivity.putExtra("nomeUsuario", getCaixaNome().getText().toString());
+        startActivity(nextActivity);
     }
 }
